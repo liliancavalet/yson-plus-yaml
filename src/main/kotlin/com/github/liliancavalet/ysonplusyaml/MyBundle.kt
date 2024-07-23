a2426ab956dc8b26a68b21d4cd3c77699b7bdb51
GitHub Action <action@github.com>
2024-07-23 23:40:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/liliancavalet/ysonplusyaml/MyBundle.kt
+++ src/main/kotlin/com/github/liliancavalet/ysonplusyaml/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.liliancavalet.ysonplusyaml
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/liliancavalet/ysonplusyaml/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liliancavalet/ysonplusyaml/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.liliancavalet.ysonplusyaml.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liliancavalet.ysonplusyaml.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
